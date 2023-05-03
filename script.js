4ff149ae2db5730d4d521bcfc32825e279f6b084
beqarion <beqarioni@yahoo.co.uk>
2023-05-03 18:01:53 +0400

    hero touched up for responsiveness

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const navToggle = document.querySelector(".nav-toggle")
 const linksContainer = document.querySelector(".links-container")
 
-navToggle.addEventListener("click", function(){
+navToggle.addEventListener("click", function () {
   linksContainer.classList.toggle("show-links")
+})
+
+// ********* auto date *********
+const date = document.querySelector(".date")
+date.innerHTML = new Date().getFullYear()
+
+// ********* fixed navbar *********
+const navbar = document.getElementById("nav")
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset
+  const navHeight = navbar.getBoundingClientRect().height
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav")
+  } else {
+    navbar.classList.remove("fixed-nav")
+  }
 })
