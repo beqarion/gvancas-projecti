490bbe8c5878b997291dba454e7edadc1b89c19b
beqarion <beqarioni@yahoo.co.uk>
2024-04-03 20:07:14 +0400

    gulp conf

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+const { watch } = require("gulp");
+const gulp = require("gulp");
+const uglify = require("gulp-uglify");
+const sourcemaps = require("gulp-sourcemaps");
+const concat = require("gulp-concat");
+const babel = require("gulp-babel");
+const browserSync = require("browser-sync").create();
+const reload = browserSync.reload;
+const eslint = require("gulp-eslint");
+
+// const cssmin = require("gulp-cssmin");
+
+const jsBuild = function () {
+  return gulp
+    .src("./*.js")
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat("all.js"))
+    .pipe(uglify())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("output/"))
+    .pipe(reload({ stream: true }));
+};
+jsBuild.displayName = "js:build";
+
+// const cssBuild = function () {
+//   return gulp
+//     .src("./*.css")
+//     .pipe(sourcemaps.init())
+//     .pipe(concat("all.css"))
+//     .pipe(cssmin())
+//     .pipe(sourcemaps.write("."))
+//     .pipe(gulp.dest("output/"))
+//     .pipe(reload({ stream: true }));
+// };
+// cssBuild.displayName = "css:build";
+
+function watchFiles() {
+  watch("./script.js", copy);
+}
+
+function copy() {
+  console.log("copy has been run");
+  return gulp
+    .src("./script.js", { sourcemaps: true })
+    .pipe(uglify())
+    .pipe(gulp.dest("output/", { sourcemaps: "." }));
+}
+function webserver() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      proxy: "beqarioni.com",
+    },
+  });
+}
+
+// watcher
+exports.watch = watchFiles;
+
+// js build (js:build) as displayName
+exports.jsBuild = jsBuild;
+
+// css build (css:build) as displayName
+// exports.cssBuild = cssBuild;
+
+// webserver
+exports.webserver = webserver;
+
+// eslint
+exports.eslint = function () {
+  return gulp
+    .src("./*.js")
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+};
